a705fb383e05430450333bd8251a31da1e422e50
Gaston Snaider <gastonsnaider93@gmail.com>
2017-03-19 18:52:28 -0700

    Problem 2 solved

--- project-euler/src/main/java/problems/problem2/Problem2.java
+++ project-euler/src/main/java/problems/problem2/Problem2.java
@@ -4,6 +4,23 @@ public class Problem2 {
 
 	private static final int MAX = 4000000;
 	
-	
+	public static void main(String[] args) {
+		
+		int prev = 1;
+		int fib = 2;
+		
+		int sum = 0;
+		
+		do {
+			if (fib % 2 == 0){
+				sum += fib;
+			}
+			int next = prev + fib;
+			prev = fib;
+			fib = next;
+		} while (fib <= MAX);
+		
+		System.out.println(sum);
+	}
 	
 }
