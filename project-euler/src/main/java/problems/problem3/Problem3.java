040c9cca922fef5e56f0f9debe8a4665b428b80e
Gaston Snaider <gastonsnaider93@gmail.com>
2017-03-19 21:34:02 -0700

    Problem 3 solved

--- project-euler/src/main/java/problems/problem3/Problem3.java
+++ project-euler/src/main/java/problems/problem3/Problem3.java
@@ -5,32 +5,38 @@ import java.util.List;
 
 public class Problem3 {
 
+//	private static final long NUM = 13195L;
 	private static final long NUM = 600851475143L;
 	private static final List<Long> primes = new ArrayList<>();
 	private static final List<Long> factors = new ArrayList<>();
 
 	public static void main(String[] args) {
 
-		final long maxPosibleFactor = NUM / 2;
-		primes.add(2L);
-		
-		for (long i = 3L; i <= maxPosibleFactor; i++) {
-
+		for (long i = 2L; i <= NUM; i++) {
 			if (isPrime(i)) {
 				primes.add(i);
 				if (NUM % i == 0) {
 					factors.add(i);
+					if (factorsMultiplyToNum()){
+						break;
+					}
 				}
 			}
 		}
-
 		System.out.println(factors.get(factors.size() - 1));
-
 	}
 
-	private static boolean isPrime(long i) {
+	private static boolean factorsMultiplyToNum() {
+		long mult = 1;
+		for (long factor : factors) {
+			mult *= factor;
+		}
+		return (mult == NUM);
+	}
+
+	private static boolean isPrime(long n) {
 		for (long prime : primes) {
-			if (i % prime == 0) {
+			if (n % prime == 0) {
 				return false;
 			}
 		}
