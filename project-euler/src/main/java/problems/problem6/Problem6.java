5c47b1d709083839228f986f8c2a67a66c95bfbf
Gaston Snaider <gastonsnaider93@gmail.com>
2017-03-19 22:02:36 -0700

    Problem 6 solved

---
+++ project-euler/src/main/java/problems/problem6/Problem6.java
@@ -0,0 +1,22 @@
+package problems.problem6;
+
+public class Problem6 {
+
+//	private static final int MAX = 10;
+	private static final int MAX = 100;
+	
+	public static void main(String[] args) {
+	
+		int sum = 0;
+		int sumOfSquares = 0;
+		
+		for (int i = 1; i <= MAX; i++) {
+			sum += i;
+			sumOfSquares += i*i;
+		}
+		
+		System.out.println(sum*sum - sumOfSquares);
+		
+	}
+
+}
