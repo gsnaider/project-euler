c494139ef64c3b896ed87ab7493d1cbfa181c03f
Gaston Snaider <gastonsnaider93@gmail.com>
2017-03-19 21:53:51 -0700

    Problems 4 and 5 solved

---
+++ project-euler/src/main/java/problems/problem5/Problem5.java
@@ -0,0 +1,28 @@
+package problems.problem5;
+
+public class Problem5 {
+
+
+//	private static final int MAX = 10;
+	private static final int MAX = 20;
+	
+	public static void main(String[] args) {
+		
+		int i = 2;
+		while(!evenlyDivisible(i)){
+			i += 2;
+		}
+		System.out.println(i);
+		
+	}
+
+	private static boolean evenlyDivisible(int num) {
+		for (int i = 1; i <= MAX; i++) {
+			if (num % i != 0){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+}
