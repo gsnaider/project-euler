ab58f0e2ee4a5abf756accc12e0d367cf1614dcc
Gaston Snaider <gastonsnaider93@gmail.com>
2017-03-22 22:42:32 -0700

    Problem 11 solved

---
+++ project-euler/src/main/java/problems/problem11/util/Constants.java
@@ -0,0 +1,12 @@
+package problems.problem11.util;
+
+public final class Constants {
+
+	public static final int ROWS = 20;
+	public static final int COLS = 20;
+	public static final int PRODUCT_FACTORS = 4;
+	
+	private Constants() {}
+	
+	
+}
