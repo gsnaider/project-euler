be875556a8a8ac12ae8ba67ecf1ce9cdf97dd449
Gaston Snaider <gastonsnaider93@gmail.com>
2017-03-23 23:05:55 -0700

    Problem 19 solved.

---
+++ project-euler/src/main/java/problems/problem19/Problem19.java
@@ -0,0 +1,27 @@
+package problems.problem19;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+
+public class Problem19 {
+
+	private static final int FIRST_YEAR = 1901;
+	private static final int LAST_YEAR = 2000;
+
+	public static void main(String[] args) {
+
+		long sundays = 0;
+		for (int year = FIRST_YEAR; year <= LAST_YEAR; year++) {
+			for (Month month : Month.values()) {
+				LocalDate date = LocalDate.of(year, month, 1);
+				if (date.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
+					sundays++;
+				}
+			}
+		}
+		System.out.println(sundays);
+
+	}
+
+}
