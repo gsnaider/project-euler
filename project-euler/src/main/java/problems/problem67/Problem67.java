bf769e077b8168c8abfd5bc03e85e41b9073adda
Gaston Snaider <gastonsnaider93@gmail.com>
2017-03-24 22:14:40 -0700

    Problems 22 and 23 solved.

--- project-euler/src/main/java/problems/problem67/Problem67.java
+++ project-euler/src/main/java/problems/problem67/Problem67.java
@@ -1,9 +1,6 @@
 package problems.problem67;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,29 +11,6 @@ import java.util.Scanner;
 import util.BinaryTreeNode;
 
 public class Problem67 {
-
-	// private static final int[][] nums =
-	// {{3},
-	// {7, 4},
-	// {2, 4, 6},
-	// {8, 5, 9, 3}};
-
-	// private static final int[][] nums =
-	// {{75},
-	// {95, 64},
-	// {17, 47, 82},
-	// {18, 35, 87, 10},
-	// {20, 4, 82, 47, 65},
-	// {19, 1, 23, 75, 3, 34},
-	// {88, 2, 77, 73, 7, 63, 67},
-	// {99, 65, 4, 28, 6, 16, 70, 92},
-	// {41, 41, 26, 56, 83, 40, 80, 70, 33},
-	// {41, 48, 72, 33, 47, 32, 37, 16, 94, 29},
-	// {53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14},
-	// {70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57},
-	// {91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48},
-	// {63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31},
-	// {04, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23}};
 
 	private static final Map<BinaryTreeNode<Integer>, Long> maxPaths = new HashMap<>();
 
---
+++ project-euler/src/main/java/util/FileUtil.java
@@ -0,0 +1,32 @@
+package util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public final class FileUtil {
+
+	private FileUtil() {
+	}
+
+	/**
+	 * Attempts to read the file located in src/main/resources/{@code filePath},
+	 * and returns all of the file lines as a List of Strings.
+	 * 
+	 * @throws FileNotFoundException if the file is not found
+	 */
+	public static List<String> readLines(String filePath) throws FileNotFoundException {
+		File file = new File(FileUtil.class.getClassLoader().getResource(filePath).getFile());
+
+		List<String> lines = new ArrayList<>();
+		Scanner scanner = new Scanner(file);
+		while (scanner.hasNextLine()) {
+			lines.add(scanner.nextLine());
+		}
+		scanner.close();
+		return lines;
+	}
+
+}
