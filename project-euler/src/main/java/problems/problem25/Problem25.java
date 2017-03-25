3370b057a8d2f39b6eb3a91f69e913c8d2b522bf
Gaston Snaider <gastonsnaider93@gmail.com>
2017-03-24 23:29:52 -0700

    Problem 25 solved.

---
+++ project-euler/src/main/java/problems/problem25/Problem25.java
@@ -0,0 +1,35 @@
+package problems.problem25;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Problem25 {
+	
+//	private static final int MAX = 3;
+	private static final int MAX = 1000;
+
+	private static final Map<Long, BigInteger> fibonacci = new HashMap<>();
+
+	public static void main(String[] args) {
+		
+		fibonacci.put(1L, BigInteger.ONE);
+		fibonacci.put(2L, BigInteger.ONE);
+		
+		long i = 3;
+		while (fibonacci(i).toString().length() < MAX) {
+			i++;
+		}
+		System.out.println(i);
+		
+	}
+
+	private static BigInteger fibonacci(long n) {
+		Long key = Long.valueOf(n);
+		if (!fibonacci.containsKey(key)) {
+			fibonacci.put(key, fibonacci(n - 1).add(fibonacci(n - 2)));
+		}
+		return fibonacci.get(key);
+	}
+
+}
