97f96b54dd109d1800e7674e5b8f6672a181fd0d
Gaston Snaider <gastonsnaider93@gmail.com>
2017-03-27 22:55:34 -0700

    Attempting problem 27

--- project-euler/src/main/java/problems/impl/problem27/Problem27.java
+++ project-euler/src/main/java/problems/impl/problem27/Problem27.java
@@ -4,6 +4,12 @@ import util.PrimeUtil;
 
 final class Problem27 {
 
+	
+//	private static final int MAX_A = 79;
+//	private static final int MIN_A = -79;
+//	private static final int MAX_B = 1601;
+//	private static final int MIN_B = 0;
+	
 	private static final int MAX_A = 999;
 	private static final int MIN_A = -999;
 	private static final int MAX_B = 1000;
@@ -16,14 +22,20 @@ final class Problem27 {
 		
 		for (int a = MIN_A; a <= MAX_A; a++) {
 			for (int b = MIN_B; b <= MAX_B; b++) {
-				int n = 0;
-				int possiblePrime = (n + a)*n + b;
-				if (PrimeUtil.isPrime(possiblePrime)){
-					
+				long n = 0;
+				long possiblePrime;
+				do {
+					possiblePrime = (n + a)*n + b;
+					n++;
+				} while (PrimeUtil.isPrime(possiblePrime));
+				long primes = n - 1;
+				if (primes > maxPrimes) {
+					maxPrimes = primes;
+					product = a*b;
 				}
-				
 			}
 		}
+		System.out.println(product);
 
 	}
 
