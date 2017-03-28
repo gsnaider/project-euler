104cdbf8fffac259abde6c5e70513b08ce575287
Gaston Snaider <gastonsnaider93@gmail.com>
2017-03-27 21:55:44 -0700

    Optimized problem 10

--- project-euler/src/main/java/problems/problem10/Problem10.java
+++ project-euler/src/main/java/problems/problem10/Problem10.java
@@ -5,20 +5,13 @@ final class Problem10 {
 //	private static final int MAX = 10;
 	private static final int MAX = 2000000;
 	
-	private static final long[] primes = new long[MAX];
-
-	
 	public static void main(String[] args) {
 		
-		primes[0] = 2;
-		int primeCount = 1;
 		long sum = 2;
 		
 		long i = 3;
 		while (i < MAX) {
-			if (isPrime(i, primeCount)) {
-				primes[primeCount] = i;
-				primeCount++;
+			if (isPrime(i)) {
 				sum += i;
 			}
 			i += 2;
@@ -29,9 +22,10 @@ final class Problem10 {
 	}
 
 
-	private static boolean isPrime(long num, int primeCount) {
-		for (int i = 0; i < primeCount; i++) {
-			if (num % primes[i] == 0) {
+	private static boolean isPrime(long num) {
+		final long sqrt = Math.round(Math.sqrt(num));
+		for (int i = 2; i <= sqrt; i++) {
+			if (num % i == 0) {
 				return false;
 			}
 		}
