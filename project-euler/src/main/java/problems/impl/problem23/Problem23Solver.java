00698ceaf6ce672e8a90c031926fff50d20d0cd0
Gaston Snaider <gastonsnaider93@gmail.com>
2017-03-28 22:57:37 -0700

    refactor

--- project-euler/src/main/java/problems/impl/problem23/Problem23Solver.java
+++ project-euler/src/main/java/problems/impl/problem23/Problem23Solver.java
@@ -8,14 +8,13 @@ import problems.ProblemSolver;
 public final class Problem23Solver implements ProblemSolver {
 
 	private static final int MAX = 28123;
-//	private static final int[] abundantNumbers = new int[MAX];
+	// private static final int[] abundantNumbers = new int[MAX];
 
 	private static final Set<Integer> abundantNumbers = new HashSet<>();
-	
+
 	@Override
 	public String solve() {
 		long notAbundantSum = 0;
-		int idx = 0;
 		for (int i = 1; i <= MAX; i++) {
 			if (isAbundant(i)) {
 				abundantNumbers.add(i);
@@ -37,28 +36,11 @@ public final class Problem23Solver implements ProblemSolver {
 		}
 		return false;
 	}
-	
-//	private static boolean isSumOfAbundant(int n) {
-//		for (int a : abundantNumbers) {
-//			if (a >= n) {
-//				break;
-//			}
-//			for (int b : abundantNumbers) {
-//				if (b > a || b > (n - a)) {
-//					break;
-//				}
-//				if (a + b == n) {
-//					return true;
-//				}
-//			}
-//		}
-//		return false;
-//	}
 
 	private static boolean isAbundant(int n) {
 		// 1 is always a proper divisor.
 		int divisorsSum = 1;
-		
+
 		long sqrt = Math.round(Math.sqrt(n));
 		for (int i = 2; i <= sqrt; i++) {
 			if (n % i == 0) {
@@ -68,10 +50,10 @@ public final class Problem23Solver implements ProblemSolver {
 		}
 
 		// Correction for perfect square.
-		if (n == sqrt*sqrt) {
+		if (n == sqrt * sqrt) {
 			divisorsSum -= sqrt;
 		}
-		
+
 		return divisorsSum > n;
 	}
 
