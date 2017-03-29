b4a5e2810760f007471774fc74ff9306ff4ec58a
Gaston Snaider <gastonsnaider93@gmail.com>
2017-03-28 22:56:46 -0700

    Problem 23 optimized

--- project-euler/src/main/java/problems/impl/problem23/Problem23Solver.java
+++ project-euler/src/main/java/problems/impl/problem23/Problem23Solver.java
@@ -1,11 +1,16 @@
 package problems.impl.problem23;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import problems.ProblemSolver;
 
 public final class Problem23Solver implements ProblemSolver {
 
 	private static final int MAX = 28123;
-	private static final int[] abundantNumbers = new int[MAX];
+//	private static final int[] abundantNumbers = new int[MAX];
+
+	private static final Set<Integer> abundantNumbers = new HashSet<>();
 	
 	@Override
 	public String solve() {
@@ -13,8 +18,10 @@ public final class Problem23Solver implements ProblemSolver {
 		int idx = 0;
 		for (int i = 1; i <= MAX; i++) {
 			if (isAbundant(i)) {
-				abundantNumbers[idx++] = i;
+				abundantNumbers.add(i);
 			}
+		}
+		for (int i = 1; i <= MAX; i++) {
 			if (!isSumOfAbundant(i)) {
 				notAbundantSum += i;
 			}
@@ -24,20 +31,29 @@ public final class Problem23Solver implements ProblemSolver {
 
 	private static boolean isSumOfAbundant(int n) {
 		for (int a : abundantNumbers) {
-			if (a >= n) {
-				break;
-			}
-			for (int b : abundantNumbers) {
-				if (b > a || b > (n - a)) {
-					break;
-				}
-				if (a + b == n) {
-					return true;
-				}
+			if (abundantNumbers.contains(n - a)) {
+				return true;
 			}
 		}
 		return false;
 	}
+	
+//	private static boolean isSumOfAbundant(int n) {
+//		for (int a : abundantNumbers) {
+//			if (a >= n) {
+//				break;
+//			}
+//			for (int b : abundantNumbers) {
+//				if (b > a || b > (n - a)) {
+//					break;
+//				}
+//				if (a + b == n) {
+//					return true;
+//				}
+//			}
+//		}
+//		return false;
+//	}
 
 	private static boolean isAbundant(int n) {
 		// 1 is always a proper divisor.
