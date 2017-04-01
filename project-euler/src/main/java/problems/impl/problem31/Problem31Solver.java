6c4026d8703775fdb22b25b7424594ed1daf02e3
Gaston Snaider <gastonsnaider93@gmail.com>
2017-04-01 16:27:55 -0700

    Adding memoization to Problem 31

--- project-euler/src/main/java/problems/impl/problem31/Problem31Solver.java
+++ project-euler/src/main/java/problems/impl/problem31/Problem31Solver.java
@@ -5,10 +5,19 @@ import problems.ProblemSolver;
 public class Problem31Solver implements ProblemSolver {
 
 	private static final int[] coins = { 200, 100, 50, 20, 10, 5, 2, 1 };
-	private static final int MONEY = 200;
-
+	private static final int MONEY = 1000;
+	private static final int[][] memo = new int[coins.length][MONEY + 1];
+	
+	private static final int UNINITIALIZED = -1;
+	
 	@Override
 	public String solve() {
+		for (int i = 0; i < memo.length; i++) {
+			for (int j = 0; j < memo[i].length; j++) {
+				memo[i][j] = UNINITIALIZED;
+			}
+		}
+		
 		int combinations = 0;
 		for (int coinIdx = 0; coinIdx < coins.length; coinIdx++) {
 			combinations += makeChangeStartingWithCoin(coinIdx, MONEY);
@@ -22,18 +31,24 @@ public class Problem31Solver implements ProblemSolver {
 	 * coins.
 	 */
 	private static int makeChangeStartingWithCoin(int coinIdx, int money) {
+		if (memo[coinIdx][money] != UNINITIALIZED) {
+			return memo[coinIdx][money];
+		}
+		int combinations;
 		int coin = coins[coinIdx];
 		if (coin == money) {
 			// Only one possibility: take only one of this coin.
-			return 1;
+			combinations = 1;
 		} else if (coin > money) {
 			// No way to form money using this coin.
-			return 0;
+			combinations = 0;
+		} else {
+			combinations = 0;
+			for (int i = coinIdx; i < coins.length; i++) {
+				combinations += makeChangeStartingWithCoin(i, money - coin);
+			}
 		}
-		int combinations = 0;
-		for (int i = coinIdx; i < coins.length; i++) {
-			combinations += makeChangeStartingWithCoin(i, money - coin);
-		}
+		memo[coinIdx][money] = combinations;
 		return combinations;
 	}
 }
