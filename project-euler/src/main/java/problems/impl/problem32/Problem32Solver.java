164a2bc77044eb9cd5acfdfc34d1dc7d982a4c11
Gaston Snaider <gastonsnaider93@gmail.com>
2017-04-02 15:26:19 -0700

    Improving problem 32

--- project-euler/src/main/java/problems/impl/problem32/Problem32Solver.java
+++ project-euler/src/main/java/problems/impl/problem32/Problem32Solver.java
@@ -1,8 +1,6 @@
 package problems.impl.problem32;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import problems.ProblemSolver;
@@ -11,30 +9,27 @@ public class Problem32Solver implements ProblemSolver {
 
 	// If any of the factors has 5 or more digits, the total number of digits of
 	// the factors would be 6 or more.
-	// The lowest product of two numbers whose digits sum 6 or more, gives as a
+	// The lowest product of two numbers whose digits count is 6 or more, gives
+	// as a
 	// 5 digit number. So the total number of digits between the factors and the
 	// product would be 11 digits in total, and that cannot be a pandigital
 	// product.
-	private static final int MAX = 9999;
+	private static final int MAX_FACTOR = 9999;
+	private static final int MAX_FACTORS_DIGITS = 5;
 
 	@Override
 	public String solve() {
 
 		final Set<Long> products = new HashSet<>();
-		final List<Integer> nonRepeatingDigits = new ArrayList<>();
 
-		for (int i = 1; i < MAX; i++) {
-			if (!hasRepeatingDigits(i)) {
-				nonRepeatingDigits.add(i);
-			}
-		}
-
-		for (int i = 0; i < nonRepeatingDigits.size(); i++) {
-			for (int j = 0; j < i; j++) {
-				int a = nonRepeatingDigits.get(i);
-				int b = nonRepeatingDigits.get(j);
-				long res = a * b;
-				if (isPanDigital(a, b, res)) {
+		for (int i = 1; i <= MAX_FACTOR; i++) {
+			for (int j = 1; j < i; j++) {
+				int factorsDigitCount = (String.valueOf(i) + String.valueOf(j)).length();
+				if (factorsDigitCount > MAX_FACTORS_DIGITS) {
+					break;
+				}
+				long res = i * j;
+				if (isPanDigital(i, j, res)) {
 					products.add(res);
 				}
 			}
@@ -45,17 +40,6 @@ public class Problem32Solver implements ProblemSolver {
 			sum += product;
 		}
 		return String.valueOf(sum);
-	}
-
-	private static boolean hasRepeatingDigits(int n) {
-		String num = String.valueOf(n);
-		for (int i = 0; i < num.length(); i++) {
-			char c = num.charAt(i);
-			if (num.indexOf(c) != num.lastIndexOf(c)) {
-				return true;
-			}
-		}
-		return false;
 	}
 
 	private boolean isPanDigital(int i, int j, long res) {
@@ -69,7 +53,5 @@ public class Problem32Solver implements ProblemSolver {
 			}
 		}
 		return true;
-
 	}
-
 }
