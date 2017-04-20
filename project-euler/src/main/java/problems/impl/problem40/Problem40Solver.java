b7b070d83935c48f87b89df08f3ee5501f62d594
Gaston Snaider <gastonsnaider93@gmail.com>
2017-04-19 22:47:44 -0700

    Improving problem 40

--- project-euler/src/main/java/problems/impl/problem40/Problem40Solver.java
+++ project-euler/src/main/java/problems/impl/problem40/Problem40Solver.java
@@ -5,26 +5,24 @@ import problems.ProblemSolver;
 public class Problem40Solver implements ProblemSolver {
 
 	private static final int MAX = 1000000;
-	
+
 	@Override
 	public String solve() throws Exception {
-		String fractional = "";
-		int i = 0;
+		int product = 1;
+		int currentDigit = 1;
+		int i = 1;
 		do {
-			fractional += i;
 			i++;
-		} while (fractional.length() <= MAX);
-		int d1 = Integer.valueOf(fractional.substring(1, 2));
-		int d10 = Integer.valueOf(fractional.substring(10, 11));
-		int d100 = Integer.valueOf(fractional.substring(100, 101));
-		int d1000 = Integer.valueOf(fractional.substring(1000, 1001));
-		int d10000 = Integer.valueOf(fractional.substring(10000, 10001));
-		int d100000 = Integer.valueOf(fractional.substring(100000, 100001));
-		int d1000000 = Integer.valueOf(fractional.substring(1000000, 1000001));
-		
+			String num = String.valueOf(i);
+			for (int d = currentDigit + 1; d <= currentDigit + num.length(); d++) {
+				if (d == 10 || d == 100 || d == 1000 || d == 10000 || d == 100000 || d == 1000000) {
+					product *= Integer.valueOf(num.substring(d - currentDigit - 1, d - currentDigit));
+				}
+			}
+			currentDigit += num.length();
+		} while (currentDigit <= MAX);
 
-		return String.valueOf(d1 * d10 * d100* d1000* d10000 * d100000 * d1000000);
+		return String.valueOf(product);
 	}
-
 
 }
