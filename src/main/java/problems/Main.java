ae73f4a5e2c5e2a921c42f4ee689bdc479b4cd81
Gaston Snaider <gaston.snaider@despegar.com>
2018-02-22 15:38:47 -0300

    First attempt at Problem 57

--- src/main/java/problems/Main.java
+++ src/main/java/problems/Main.java
@@ -1,11 +1,11 @@
 package problems;
 
-import problems.impl.problem56.Problem56Solver;
+import problems.impl.problem57.Problem57Solver;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		ProblemSolver problemSolver = new Problem56Solver();
+		ProblemSolver problemSolver = new Problem57Solver();
 		System.out.println(problemSolver.solve());
 	}
 
