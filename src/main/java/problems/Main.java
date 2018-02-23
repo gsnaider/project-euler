fdab8209d66be8ab83b1bc826c5f0d2fd6adc3ab
Gaston Snaider <gaston.snaider@despegar.com>
2018-02-23 12:54:06 -0300

    Starting problem 59

--- src/main/java/problems/Main.java
+++ src/main/java/problems/Main.java
@@ -1,11 +1,11 @@
 package problems;
 
-import problems.impl.problem58.Problem58Solver;
+import problems.impl.problem59.Problem59Solver;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		 ProblemSolver problemSolver = new Problem58Solver();
+		 ProblemSolver problemSolver = new Problem59Solver();
 		 System.out.println(problemSolver.solve());
 	}
 
---
+++ src/main/java/problems/impl/problem59/Problem59Solver.java
@@ -0,0 +1,14 @@
+package problems.impl.problem59;
+
+import problems.ProblemSolver;
+
+public class Problem59Solver implements ProblemSolver {
+
+	@Override
+	public String solve() {
+		
+		return String.valueOf(null);
+	}
+
+
+}
