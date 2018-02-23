ef7c76f7d75bd01bdb9372e4e77df63a70e5326b
Gaston Snaider <gaston.snaider@despegar.com>
2018-02-23 10:50:17 -0300

    Problem 57 solved

--- src/main/java/problems/Main.java
+++ src/main/java/problems/Main.java
@@ -5,8 +5,8 @@ import problems.impl.problem57.Problem57Solver;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		ProblemSolver problemSolver = new Problem57Solver();
-		System.out.println(problemSolver.solve());
+		 ProblemSolver problemSolver = new Problem57Solver();
+		 System.out.println(problemSolver.solve());
 	}
 
 }
--- src/main/java/problems/impl/problem57/Problem57Solver.java
+++ src/main/java/problems/impl/problem57/Problem57Solver.java
@@ -11,17 +11,14 @@ public class Problem57Solver implements ProblemSolver {
 	@Override
 	public String solve() {
 		
-		double previous = 0;
+		BigFraction previous = BigFraction.ZERO;
 		int result = 0;
 		for (int i = 0; i < EXPANSIONS; i++) {
-			double expansion = 1 + 1/(2 + previous);
-			previous = expansion - 1;
-			
-			BigFraction fraction = new BigFraction(expansion);
-			if (fraction.getNumerator().toString().length() > fraction.getDenominator().toString().length()) {
+			BigFraction expansion = BigFraction.ONE.add(BigFraction.ONE.divide(BigFraction.TWO.add(previous)));
+			previous = expansion.subtract(BigFraction.ONE);
+			if (expansion.getNumerator().toString().length() > expansion.getDenominator().toString().length()) {
 				result++;
 			}
-			
 		}
 		return String.valueOf(result);
 	}
