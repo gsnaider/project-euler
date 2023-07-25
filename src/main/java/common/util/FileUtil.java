b7ad0679047afd72aeda9b442b072f0c1ff658f7
Gaston <gnsnaider@gmail.com>
2023-07-25 16:06:40 +0200

    Adding TODO for fixing compilation error

--- src/main/java/common/util/FileUtil.java
+++ src/main/java/common/util/FileUtil.java
@@ -43,7 +43,8 @@ public final class FileUtil {
 	 *             if the file is not found
 	 */
 	public static List<List<String>> readCsvFile(String filePath) throws FileNotFoundException {
-		return readLines(filePath).stream().map(line -> Lists.newArrayList(line.split(","))).collect(Collectors.toList());
+		// TODO: fix compilation error
+		return readLines(filePath).stream().map(line -> Lists.newArrayList(line.split(","))).collect(Collectors.toList<>());
 	}
 
 }
